bb2f1c85bc2aeef276d38be647ab54d07096fa55
STQT <productgsm@gmail.com>
2025-12-04 22:33:48 +0500

    cors changes

--- config/settings/base.py
+++ config/settings/base.py
@@ -29,6 +29,7 @@ CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",
     "http://127.0.0.1:4200",
+    "https://api.acham.uz",
 ]
 CORS_ALLOW_CREDENTIALS = True
 
